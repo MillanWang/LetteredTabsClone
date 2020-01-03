f0f0c88fd6e9005c22eae096befd23a8ba836054
MillanWang <real.millan.wang@gmail.com>
2020-01-03 18:37:14 -0500

    Nice

--- frontend/api.js
+++ frontend/api.js
@@ -1,16 +1,8 @@
 function convert() {
     var tabs = document.getElementById("tabs").value;
     var xhr = new XMLHttpRequest();
-    var host = window.location.hostname;
-    // Add port if running locally
-    var url = '';
-    if (host === "localhost") {
-        url = 'http://localhost:5000/conversion';
-    } else {
-        url = 'https://lettered-tabs.herokuapp.com/conversion';
-    }
 
-    xhr.open("POST", url, true);
+    xhr.open("POST", `${window.location.href}conversion`, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         input: tabs
